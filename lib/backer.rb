4c765f37ddf6bbc6cb2aabd8d4733382c040ffe0
Mary Jack <maryjackinthebox@gmail.com>
2018-05-04 02:10:16 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,5 @@
 class Backer
-  attr_accessor :name, :projects
+  attr_accessor :name, :project
  
   @@project_count = 0
   
@@ -10,13 +10,6 @@ class Backer
  
   def back_project(project)
     @project << project
-  
   end
-  
-  #def add_post_by_title(post_title)
-
-   # post = Post.new("#{post_title}") #error occured because I forgot to capitalize the P. Necessary for class methods
-#end
-    
 
 end
