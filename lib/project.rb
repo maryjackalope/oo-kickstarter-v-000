4c3ecd1cfd27f9d35f5d4d8d07862b6e247cc6c3
Mary Jack <maryjackinthebox@gmail.com>
2018-05-04 02:08:16 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,10 @@ class Project
   end
   
   def add_backer(backer)
+    @backer << backer
+    post.author = self
+    @@post_count += 1
+  end
     backed_projects = [] >> backer
     add_backer(backer)
   end 
