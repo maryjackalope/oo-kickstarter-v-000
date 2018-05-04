9e32a7102e36ba4d6b9a5e31a96467c5612cbaf8
Mary Jack <maryjackinthebox@gmail.com>
2018-05-04 02:00:41 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,1 +1,29 @@
+class Author
+  attr_accessor :name, :posts
+ 
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+ 
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(post_title)
+<<<<<<< HEAD
+    post = Post.new("#{post_title}") #error occured because I forgot to capitalize the P. Necessary for class methods
+=======
+    post = post.new("#{post_title}")
+>>>>>>> 4324ae6a4a66dded4e7488eb4bea0bc9ee1bec63
+    add_post(post)
+  end
 
+  def self.post_count
+    @@post_count
+  end
+end
